f9396d1d18af37c09ea7065ee231e4ea05736f58
Samyak Kamble <126759819+samyak2403@users.noreply.github.com>
2023-08-06 01:05:52 +0530

    Initial commit

---
+++ admobopenads/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.arrowwould.admobopenads"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
+
+
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime:2.0.0")
+    annotationProcessor("androidx.lifecycle:lifecycle-compiler:2.0.0")
+}
---
+++ app/src/main/java/com/arrowwould/admobopenads/MyApplication.java
@@ -0,0 +1,18 @@
+package com.arrowwould.admobopenads;
+
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(this);
+
+        new AppOpenManager(this, "ca-app-pub-3940256099942544/3419835294");
+
+    }
+
+}
